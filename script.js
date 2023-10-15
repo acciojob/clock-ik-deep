f8c7c33bb5119d5ab299cc0dd6c162c7e4bd798b
Irfan khan <80601941+ik-deep@users.noreply.github.com>
2023-10-15 16:14:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+var currentdate = new Date(); 
+var datetime = currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " , "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+
+let p = document.getElementById("timer");
+p.innerText = datetime
