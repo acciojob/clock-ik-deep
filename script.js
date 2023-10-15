ede3fffdb87cb7eaaf8d829ad97f769904dd5897
Irfan khan <80601941+ik-deep@users.noreply.github.com>
2023-10-15 16:21:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 //your JS code here. If required.
-var currentdate = new Date(); 
+let p = document.getElementById('timer')
+function getTimeAndDate(){
+	var currentdate = new Date(); 
 var datetime = currentdate.getDate() + "/"
                 + (currentdate.getMonth()+1)  + "/" 
                 + currentdate.getFullYear() + " , "  
                 + currentdate.getHours() + ":"  
                 + currentdate.getMinutes() + ":" 
                 + currentdate.getSeconds();
+	p.innerText=datetime;
+}
 
-let p = document.getElementById("timer");
-p.innerText = datetime
+setInterval(getTimeAndDate,1000 )
+
